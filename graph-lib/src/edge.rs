a216785c2d03b6146dd97574f9542adac8187f55
alfecki <alexskvr03@gmail.com>
2024-04-07 20:26:47 +0300

    edge update

--- graph-lib/src/edge.rs
+++ graph-lib/src/edge.rs
@@ -1,18 +1,41 @@
 pub mod edge {
-    use std::rc::Rc;
+    use std::rc::{Rc, Weak};
     use crate::graph::graph::DefaultGraph;
     use crate::vertex::vertex::DefaultVertex;
 
-    pub trait DefaultEdge {
-        fn end<T>() -> Rc<&dyn DefaultVertex<T>>;
+    pub trait DefaultEdge<T> {
+        fn end() -> Weak<&dyn DefaultVertex<T>>;
     }
 
-    pub trait DefaultBidirectionalEdge: DefaultEdge {
-        fn start<T>() -> Rc<&dyn DefaultVertex<T>>;
+    pub trait DefaultBidirectionalEdge<T>: DefaultEdge<T> {
+        fn start() ->  Weak<&dyn DefaultVertex<T>>;
     }
 
-    pub struct Edge {
+    pub struct Edge<T> {
+        end: Weak<dyn DefaultVertex<T>>
+    }
 
+    impl<T> DefaultEdge<T> for Edge<T> {
+        fn end() -> Weak<&dyn DefaultVertex<T>> {
+            Self.end
+        }
+    }
+
+    pub struct BidirectionalEdge<T> {
+        start: Weak<dyn DefaultVertex<T>>,
+        end: Weak<dyn DefaultVertex<T>>
+    }
+
+
+    impl<T> DefaultEdge<T> for BidirectionalEdge<T> {
+        fn end() -> Weak<&dyn DefaultVertex<T>> {
+            Self.end
+        }
+    }
+    impl<T> DefaultBidirectionalEdge<T> for BidirectionalEdge<T> {
+        fn start() ->  Weak<&dyn DefaultVertex<T>> {
+            Self.start
+        }
     }
 
 }
