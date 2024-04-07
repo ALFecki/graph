88f793148fc16aee597fc8459810338df15dd7cd
alfecki <alexskvr03@gmail.com>
2024-04-07 21:21:05 +0300

    graph update

--- graph-lib/src/graph.rs
+++ graph-lib/src/graph.rs
@@ -1,21 +1,22 @@
 pub mod graph {
+    use std::collections::HashSet;
     use std::rc::Rc;
-    use crate::vertex::vertex::DefaultVertex;
+    use crate::edge::edge::{DefaultEdge, Edge};
+    use crate::vertex::vertex::{DefaultVertex, Vertex};
 
     pub trait DefaultGraph<T> {
         fn vertex_count() -> usize;
-        fn edges_count();
+        fn edges_count() -> usize;
         fn get_vertexes() -> Vec<Rc<dyn DefaultVertex<T>>>;
-        fn add_edge(start: Rc<dyn DefaultVertex<T>>, end: Rc<dyn DefaultVertex<T>>);
+
+        fn add_edge(edge: Rc<dyn DefaultEdge<T>>);
+        fn add_edge_with_vertexes(start: Rc<dyn DefaultVertex<T>>, end: Rc<dyn DefaultVertex<T>>);
 
     }
-
-    pub trait OrientedDefault<T>: DefaultGraph<T> {
-    }
-
 
     pub struct Graph<T> {
-        vertexes: Vec<Rc<dyn DefaultVertex<T>>>
+        vertexes: HashSet<Rc<dyn DefaultVertex<T>>>,
+        edges: Vec<Rc<Edge<T>>>
     }
 
     impl<T> DefaultGraph<T> for Graph<T> {
@@ -23,16 +24,21 @@ pub mod graph {
             Self.vertexes.len()
         }
 
-        fn edges_count() {
-            todo!()
+        fn edges_count() -> usize {
+            Self.edges.len()
         }
 
         fn get_vertexes() -> Vec<Rc<dyn DefaultVertex<T>>> {
-            Self.vertexes
+            todo!()
         }
 
-        fn add_edge(start: Rc<dyn DefaultVertex<T>>, end: Rc<dyn DefaultVertex<T>>) {
-            todo!()
+        fn add_edge(edge: Rc<dyn DefaultEdge<T>>) {
+            Self.edges.push(edge)
+        }
+
+        fn add_edge_with_vertexes(start: Rc<dyn DefaultVertex<T>>, end: Rc<dyn DefaultVertex<T>>) {
+
+
         }
     }
 }
