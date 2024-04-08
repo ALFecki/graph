391f20d7b7141fabceac47ab11cf2b18e53818e3
alfecki <alexskvr03@gmail.com>
2024-04-09 00:41:48 +0300

    updates for graph structure

--- graph-lib/src/graph.rs
+++ graph-lib/src/graph.rs
@@ -1,55 +1,64 @@
 pub mod graph {
-    use std::collections::HashSet;
-    use std::rc::{Rc, Weak};
     use crate::edge::edge::{DefaultEdge, Edge, OrientedEdge};
     use crate::vertex::vertex::{DefaultVertex, Vertex};
+    use std::collections::HashSet;
+    use std::fmt::Error;
+    use std::rc::{Rc, Weak};
 
     pub trait DefaultGraph<T> {
+        type VertexType: DefaultVertex<T>;
+        type EdgeType: DefaultEdge<T>;
         fn vertex_count(&self) -> usize;
         fn edges_count(&self) -> usize;
-        fn get_vertexes(&self) -> Vec<impl DefaultVertex<T>>;
+        fn get_vertexes(&self) -> Vec<Rc<Self::VertexType>>;
 
-        fn get_vertex_by_id(&self, id: usize) -> impl DefaultVertex<T>;
+        fn get_vertex_by_id(&self, id: usize) -> Option<&Rc<Self::VertexType>>;
 
-        fn add_edge(&mut self, edge: impl DefaultEdge<T>);
-        fn add_edge_with_vertexes(&mut self, start: impl DefaultVertex<T>, end: impl DefaultVertex<T>);
-
+        fn add_edge(&mut self, edge: Self::EdgeType);
+        fn add_edge_with_vertex_id(&mut self, start: usize, end: usize) -> Result<(), String>;
     }
 
     pub struct OrientedGraph<T> {
-        vertexes: Vec<Vertex<T, OrientedEdge<T, V>>>,
-        // edges: Vec<OrientedEdge<T>>>>
+        vertexes: Vec<Rc<Vertex<T, OrientedEdge<T>>>>,
+        edges: Vec<Rc<OrientedEdge<T>>>,
     }
 
     impl<T> DefaultGraph<T> for OrientedGraph<T> {
+        type VertexType = Vertex<T, OrientedEdge<T>>;
+        type EdgeType = OrientedEdge<T>;
+
         fn vertex_count(&self) -> usize {
-            Self.vertexes.len()
+            self.vertexes.len()
         }
 
         fn edges_count(&self) -> usize {
             self.edges.len()
         }
 
-        fn get_vertexes(&self) -> Vec<Vertex<T, OrientedEdge<T>>> {
-            todo!()
+        fn get_vertexes(&self) -> Vec<Rc<Self::VertexType>> {
+            self.vertexes.clone()
         }
 
-        fn get_vertex_by_id(&self, id: usize) -> Vertex<T, OrientedEdge<T>> {
-            todo!()
+        fn get_vertex_by_id(&self, id: usize) -> Option<&Rc<Self::VertexType>> {
+            self.vertexes.iter().find(|&&p| p.get_id() == id)
         }
 
-        fn add_edge(&mut self, edge: impl DefaultEdge<T>) {
-            self.edges.push(edge)
+        fn add_edge(&mut self, edge: Self::EdgeType) {
+            self.edges.push(Rc::new(edge))
         }
 
-        fn add_edge_with_vertexes(&mut self, start: impl DefaultVertex<T>, end: impl DefaultVertex<T>) {
-            self.edges.push(
-                OrientedEdge::<T> {
-                    start: Weak::new(start),
-                    end: Weak::new(end)
-                }
-            )
-
+        fn add_edge_with_vertex_id(&mut self, start: usize, end: usize) -> Result<(), String> {
+            if let (Some(mut start), Some(mut end)) =
+                (self.get_vertex_by_id(start), self.get_vertex_by_id(end))
+            {
+                let new_edge = Rc::new(OrientedEdge::<T>::new(start.clone(), end.clone()));
+                start.add_neighbor(new_edge.clone());
+                end.add_neighbor(new_edge.clone());
+                self.edges.push(new_edge);
+                Ok(())
+            } else {
+                Err(String::from("Cannot find vertexes"))
+            }
         }
     }
 }
