36dd5a2d4ea0ac10d7008850cb368b130e4be65f
Skvortsov A.V <alexskvr03@gmail.com>
2024-04-08 17:09:26 +0300

    updated graph trait

--- graph-lib/src/graph.rs
+++ graph-lib/src/graph.rs
@@ -9,6 +9,8 @@ pub mod graph {
         fn edges_count() -> usize;
         fn get_vertexes() -> Vec<Rc<dyn DefaultVertex<T>>>;
 
+        fn get_vertex_by_id(id: usize) -> dyn DefaultVertex<T>;
+
         fn add_edge(edge: Rc<dyn DefaultEdge<T>>);
         fn add_edge_with_vertexes(start: Rc<dyn DefaultVertex<T>>, end: Rc<dyn DefaultVertex<T>>);
 
