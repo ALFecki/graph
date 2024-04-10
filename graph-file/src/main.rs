b9969ce85eb656597e94a9b6520d9dd69e46b44c
ALFecki <alexskvr03@gmail.com>
2024-04-10 23:51:19 +0300

    binary is ready

--- graph-file/src/main.rs
+++ graph-file/src/main.rs
@@ -1,33 +1,25 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{Read, stdin, stdout, Write};
 use graph_lib::graph::graph::{DefaultGraph, OrientedGraph};
 use graph_lib::serde::serde_graph::{DeserializeGraph, SerializeGraph};
 
-fn main() {
-    let s = concat!(
-        "1 First node\n",
-        "2 Second node\n",
-        "#\n",
-        "1 2 Edge between the two\n"
-    );
+fn main() -> Result<(), Box<dyn Error>>{
+    println!("Enter the filename: ");
+    let mut filename = String::new();
 
-    let mut graph = OrientedGraph::<String, String>::deserialize(s).unwrap();
-    println!("{:?}", graph);
-    graph.add_raw_vertex(3, "Test".to_string());
-    // graph
-    //     .add_edge_with_vertex_id(1, 2, "Edge 1-2".to_string())
-    //     .unwrap();
-    graph
-        .add_edge_with_vertex_id(2, 3, Some("Edge 2-3".to_string()))
-        .unwrap();
-    graph
-        .add_edge_with_vertex_id(1, 3, Some("Edge 1-3".to_string()))
-        .unwrap();
-    graph
-        .add_edge_with_vertex_id(3, 1, Some("Edge 3-1".to_string()))
-        .unwrap();
-    let result = graph.depth_first_search(1).unwrap();
-    println!("{}", result);
-    // graph.remove_vertex_by_id(2);
+    stdin().read_line(&mut filename)?;
+    
+    let mut file = File::open(filename.trim_end())?;
 
-    let ser_graph = graph.serialize().unwrap();
-    println!("{}", ser_graph)
+    let mut serialized_graph = String::new();
+
+    file.read_to_string(&mut serialized_graph)?;
+
+    let graph = OrientedGraph::<String, String>::deserialize(serialized_graph.as_str()).unwrap();
+
+    let dfs_result = graph.depth_first_search(1).unwrap();
+    println!("{}", dfs_result);
+
+    Ok(())
 }
