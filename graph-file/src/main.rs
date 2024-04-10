0504ba72854ceb690fc31c3de5fdb10de5e83276
ALFecki <alexskvr03@gmail.com>
2024-04-10 23:28:40 +0300

    some tests

--- graph-file/src/main.rs
+++ graph-file/src/main.rs
@@ -12,18 +12,21 @@ fn main() {
     let mut graph = OrientedGraph::<String, String>::deserialize(s).unwrap();
     println!("{:?}", graph);
     graph.add_raw_vertex(3, "Test".to_string());
+    // graph
+    //     .add_edge_with_vertex_id(1, 2, "Edge 1-2".to_string())
+    //     .unwrap();
     graph
-        .add_edge_with_vertex_id(1, 2, "Edge 1-2".to_string())
+        .add_edge_with_vertex_id(2, 3, Some("Edge 2-3".to_string()))
         .unwrap();
     graph
-        .add_edge_with_vertex_id(2, 3, "Edge 2-3".to_string())
+        .add_edge_with_vertex_id(1, 3, Some("Edge 1-3".to_string()))
         .unwrap();
     graph
-        .add_edge_with_vertex_id(1, 3, "Edge 1-3".to_string())
+        .add_edge_with_vertex_id(3, 1, Some("Edge 3-1".to_string()))
         .unwrap();
     let result = graph.depth_first_search(1).unwrap();
-    println!("{}", result.to_string());
-    graph.remove_vertex_by_id(2);
+    println!("{}", result);
+    // graph.remove_vertex_by_id(2);
 
     let ser_graph = graph.serialize().unwrap();
     println!("{}", ser_graph)
